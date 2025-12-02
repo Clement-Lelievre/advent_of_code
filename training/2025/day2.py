72d6f8144db290771a7ecc255934212347ac6354
Clement <clement@rayon.so>
2025-12-02 09:20:20 +0100

    used a REGEX for day 2 part 2

--- training/2025/day2.py
+++ training/2025/day2.py
@@ -1,8 +1,11 @@
+import re
+
 example = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124"""
 
 # looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+PAT = re.compile(r"^(\d+)\1+$")
 
 
 def solve(inp: str) -> int:
@@ -20,7 +23,23 @@ def solve(inp: str) -> int:
     return ans
 
 
+def solve_p2(inp: str) -> int:
+    ans = 0
+    for r in inp.split(","):
+        start, end = r.split("-")
+        for nb in map(str, range(int(start), int(end) + 1)):
+            if PAT.search(nb):
+                ans += int(nb)
+    print(ans)
+    return ans
+
+
 if __name__ == "__main__":
     assert solve(example) == 1227775554
     with open("data/day2.txt", "r") as f:
         solve(f.read())
+    assert not PAT.match("34345")
+    assert PAT.match("4343")
+    assert (s := solve_p2(example)) == 4_174_379_265, s
+    with open("data/day2.txt", "r") as f:
+        solve_p2(f.read())
