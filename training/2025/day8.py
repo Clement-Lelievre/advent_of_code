80020977da8794d23be458a72e628d187cc95bf0
Clement <clement@rayon.so>
2025-12-09 19:36:16 +0100

    day 8 part 2

--- training/2025/day8.py
+++ training/2025/day8.py
@@ -68,11 +68,38 @@ def p1(inp: str, nb_connections: int) -> int:
 def p1_with_clustering(): ...
 
 
-def p2(inp: str) -> int: ...
+def p2(inp: str) -> int:
+    points = [
+        Point(map(int, line.split(","))) for line in inp.splitlines() if line.strip()
+    ]
+    connections = sorted(
+        combinations(points, 2), key=lambda x: three_d_euclidean_distance(x[0], x[1])
+    )
+    circuits: list[set[Point]] = [{pt} for pt in points]
+    prev_nb_circuits = 0
+    for ind, conn in enumerate(connections):
+        pt_a, pt_b = conn
+        circuits_to_merge: list[int] = []
+        for ind, circuit in enumerate(circuits):
+            if pt_a in circuit or pt_b in circuit:
+                circuits_to_merge.append(ind)
+        merged_circuit = {pt_a, pt_b}
+        for i in circuits_to_merge:
+            merged_circuit |= circuits[i]
+        circuits = [
+            c for i, c in enumerate(circuits) if i not in circuits_to_merge
+        ] + [merged_circuit]
+        if len(circuits) == 1 and prev_nb_circuits != 1:
+            ans = pt_a[0] * pt_b[0]
+            prev_nb_circuits = 1
+        elif len(circuits) > 1:
+            prev_nb_circuits = len(circuits)
+    print(ans)
+    return ans
 
 
 if __name__ == "__main__":
-    assert p1(example, 10) == 40
-    p1(open("data/day8.txt", "r").read(), 1000)
+    # assert p1(example, 10) == 40
+    # p1(open("data/day8.txt", "r").read(), 1000)
     assert p2(example) == 25_272
     p2(open("data/day8.txt", "r").read())
--- training/2025/day9.py
+++ training/2025/day9.py
@@ -14,6 +14,9 @@ Point = namedtuple("Point", ["x", "y"])
 
 
 def p1(inp: str) -> int:
+    """Naive O(n^2) search, but oh well that scaled.
+    If it hadn't, then I'd have tried creating a grid to rule out some pairs
+    """
     points = [
         Point(int(elem.split(",")[0]), int(elem.split(",")[1]))
         for elem in inp.splitlines()
@@ -25,7 +28,18 @@ def p1(inp: str) -> int:
     print(ans)
     return ans
 
+def p2(inp:str)->int:
+    # for each row in the grid, determine which ranges of columns make up the area
+    
+    # for each pair of tiles, check if all four corners of the rectangle the pair creates are in the area
+    # if yes, then update the max area
+    ans =0
+    print(ans)
+    return ans
+    
 
 if __name__ == "__main__":
     assert p1(example) == 50
     p1(open("data/day9.txt", "r").read())
+    assert p2(example)==24
+    p2(open("data/day9.txt", "r").read())
